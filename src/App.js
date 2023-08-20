2107d869092f8f75c60feb5424fe1685f2111966
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-08-20 23:05:25 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import Customer from "./component/customer";
 import Empower from "./component/empower";
-import Faq from "./component/faq";
-import Footer from "./component/footer";
+// import Faq from "./component/faq";
+// import Footer from "./component/footer";
 import Herobanner from "./component/herobanner";
 import Navbar from "./component/navbar";
-import Newsletter from "./component/newsletter";
+// import Newsletter from "./component/newsletter";
 import Partner from "./component/partners";
 import Service from "./component/service";
-import Testimonial from "./component/testimonial";
+// import Testimonial from "./component/testimonial";
 
 function App() {
   return (
